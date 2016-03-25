5484f1d91a632acf0a8b9b083457e1f5f7ae11d7
AndreyZvezdin <profforg@inbox.ru>
2016-03-25 21:33:33 +0300

    http/ftp downloader

---
+++ 3CX_FTP_Downloader/MainForm.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace _3CX_FTP_Downloader
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.IDPB_DOWNLOADPROGRESS = new System.Windows.Forms.ProgressBar();
+            this.lblUrl = new System.Windows.Forms.Label();
+            this.lblTransferRate = new System.Windows.Forms.Label();
+            this.lblEstimatedTime = new System.Windows.Forms.Label();
+            this.IDT_URL = new System.Windows.Forms.TextBox();
+            this.IDB_DOWNLOAD = new System.Windows.Forms.Button();
+            this.lblDownloadedBytes = new System.Windows.Forms.Label();
+            this.IDT_STATUS = new System.Windows.Forms.TextBox();
+            this.IDT_TRANSFERRATE = new System.Windows.Forms.TextBox();
+            this.IDT_TIMELEFT = new System.Windows.Forms.TextBox();
+            this.IDT_DOWNLOADEDBYTES = new System.Windows.Forms.TextBox();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.IDT_SAVETO = new System.Windows.Forms.TextBox();
+            this.IDB_SAVETO = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // IDPB_DOWNLOADPROGRESS
+            // 
+            this.IDPB_DOWNLOADPROGRESS.Location = new System.Drawing.Point(12, 100);
+            this.IDPB_DOWNLOADPROGRESS.Name = "IDPB_DOWNLOADPROGRESS";
+            this.IDPB_DOWNLOADPROGRESS.Size = new System.Drawing.Size(513, 23);
+            this.IDPB_DOWNLOADPROGRESS.TabIndex = 0;
+            // 
+            // lblUrl
+            // 
+            this.lblUrl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblUrl.Location = new System.Drawing.Point(12, 20);
+            this.lblUrl.Name = "lblUrl";
+            this.lblUrl.Size = new System.Drawing.Size(100, 23);
+            this.lblUrl.TabIndex = 1;
+            this.lblUrl.Text = "URL:";
+            this.lblUrl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTransferRate
+            // 
+            this.lblTransferRate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTransferRate.Location = new System.Drawing.Point(12, 140);
+            this.lblTransferRate.Name = "lblTransferRate";
+            this.lblTransferRate.Size = new System.Drawing.Size(100, 23);
+            this.lblTransferRate.TabIndex = 2;
+            this.lblTransferRate.Text = "Transfer rate:";
+            this.lblTransferRate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblEstimatedTime
+            // 
+            this.lblEstimatedTime.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblEstimatedTime.Location = new System.Drawing.Point(12, 180);
+            this.lblEstimatedTime.Name = "lblEstimatedTime";
+            this.lblEstimatedTime.Size = new System.Drawing.Size(100, 23);
+            this.lblEstimatedTime.TabIndex = 3;
+            this.lblEstimatedTime.Text = "Estimated time left:";
+            this.lblEstimatedTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // IDT_URL
+            // 
+            this.IDT_URL.Location = new System.Drawing.Point(118, 22);
+            this.IDT_URL.Name = "IDT_URL";
+            this.IDT_URL.Size = new System.Drawing.Size(326, 20);
+            this.IDT_URL.TabIndex = 4;
+            // 
+            // IDB_DOWNLOAD
+            // 
+            this.IDB_DOWNLOAD.Location = new System.Drawing.Point(450, 20);
+            this.IDB_DOWNLOAD.Name = "IDB_DOWNLOAD";
+            this.IDB_DOWNLOAD.Size = new System.Drawing.Size(75, 23);
+            this.IDB_DOWNLOAD.TabIndex = 5;
+            this.IDB_DOWNLOAD.Text = "Download";
+            this.IDB_DOWNLOAD.UseVisualStyleBackColor = true;
+            this.IDB_DOWNLOAD.Click += new System.EventHandler(this.IDB_DOWNLOAD_Click);
+            // 
+            // lblDownloadedBytes
+            // 
+            this.lblDownloadedBytes.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDownloadedBytes.Location = new System.Drawing.Point(12, 220);
+            this.lblDownloadedBytes.Name = "lblDownloadedBytes";
+            this.lblDownloadedBytes.Size = new System.Drawing.Size(100, 23);
+            this.lblDownloadedBytes.TabIndex = 6;
+            this.lblDownloadedBytes.Text = "Downloaded bytes:";
+            this.lblDownloadedBytes.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // IDT_STATUS
+            // 
+            this.IDT_STATUS.Enabled = false;
+            this.IDT_STATUS.Location = new System.Drawing.Point(12, 260);
+            this.IDT_STATUS.Multiline = true;
+            this.IDT_STATUS.Name = "IDT_STATUS";
+            this.IDT_STATUS.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.IDT_STATUS.Size = new System.Drawing.Size(513, 72);
+            this.IDT_STATUS.TabIndex = 7;
+            // 
+            // IDT_TRANSFERRATE
+            // 
+            this.IDT_TRANSFERRATE.Enabled = false;
+            this.IDT_TRANSFERRATE.Location = new System.Drawing.Point(118, 142);
+            this.IDT_TRANSFERRATE.Name = "IDT_TRANSFERRATE";
+            this.IDT_TRANSFERRATE.Size = new System.Drawing.Size(154, 20);
+            this.IDT_TRANSFERRATE.TabIndex = 8;
+            // 
+            // IDT_TIMELEFT
+            // 
+            this.IDT_TIMELEFT.Enabled = false;
+            this.IDT_TIMELEFT.Location = new System.Drawing.Point(118, 182);
+            this.IDT_TIMELEFT.Name = "IDT_TIMELEFT";
+            this.IDT_TIMELEFT.Size = new System.Drawing.Size(154, 20);
+            this.IDT_TIMELEFT.TabIndex = 9;
+            // 
+            // IDT_DOWNLOADEDBYTES
+            // 
+            this.IDT_DOWNLOADEDBYTES.Enabled = false;
+            this.IDT_DOWNLOADEDBYTES.Location = new System.Drawing.Point(118, 222);
+            this.IDT_DOWNLOADEDBYTES.Name = "IDT_DOWNLOADEDBYTES";
+            this.IDT_DOWNLOADEDBYTES.Size = new System.Drawing.Size(154, 20);
+            this.IDT_DOWNLOADEDBYTES.TabIndex = 10;
+            // 
+            // lblPath
+            // 
+            this.lblPath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblPath.Location = new System.Drawing.Point(12, 60);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(100, 23);
+            this.lblPath.TabIndex = 11;
+            this.lblPath.Text = "Save to:";
+            this.lblPath.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // IDT_SAVETO
+            // 
+            this.IDT_SAVETO.Location = new System.Drawing.Point(118, 62);
+            this.IDT_SAVETO.Name = "IDT_SAVETO";
+            this.IDT_SAVETO.Size = new System.Drawing.Size(326, 20);
+            this.IDT_SAVETO.TabIndex = 12;
+            // 
+            // IDB_SAVETO
+            // 
+            this.IDB_SAVETO.Location = new System.Drawing.Point(450, 62);
+            this.IDB_SAVETO.Name = "IDB_SAVETO";
+            this.IDB_SAVETO.Size = new System.Drawing.Size(33, 23);
+            this.IDB_SAVETO.TabIndex = 13;
+            this.IDB_SAVETO.Text = "...";
+            this.IDB_SAVETO.UseVisualStyleBackColor = true;
+            this.IDB_SAVETO.Click += new System.EventHandler(this.IDB_SAVETO_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 336);
+            this.Controls.Add(this.IDB_SAVETO);
+            this.Controls.Add(this.IDT_SAVETO);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.IDT_DOWNLOADEDBYTES);
+            this.Controls.Add(this.IDT_TIMELEFT);
+            this.Controls.Add(this.IDT_TRANSFERRATE);
+            this.Controls.Add(this.IDT_STATUS);
+            this.Controls.Add(this.lblDownloadedBytes);
+            this.Controls.Add(this.IDB_DOWNLOAD);
+            this.Controls.Add(this.IDT_URL);
+            this.Controls.Add(this.lblEstimatedTime);
+            this.Controls.Add(this.lblTransferRate);
+            this.Controls.Add(this.lblUrl);
+            this.Controls.Add(this.IDPB_DOWNLOADPROGRESS);
+            this.Name = "MainForm";
+            this.Text = "3CX_FTP_Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar IDPB_DOWNLOADPROGRESS;
+        private System.Windows.Forms.Label lblUrl;
+        private System.Windows.Forms.Label lblTransferRate;
+        private System.Windows.Forms.Label lblEstimatedTime;
+        private System.Windows.Forms.TextBox IDT_URL;
+        private System.Windows.Forms.Button IDB_DOWNLOAD;
+        private System.Windows.Forms.Label lblDownloadedBytes;
+        private System.Windows.Forms.TextBox IDT_STATUS;
+        private System.Windows.Forms.TextBox IDT_TRANSFERRATE;
+        private System.Windows.Forms.TextBox IDT_TIMELEFT;
+        private System.Windows.Forms.TextBox IDT_DOWNLOADEDBYTES;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.TextBox IDT_SAVETO;
+        private System.Windows.Forms.Button IDB_SAVETO;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ 3CX_FTP_Downloader/MainForm.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3CX_FTP_Downloader
+{
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// WebClient that will be doing the downloading
+        /// </summary>
+        WebClient m_WebClient;          
+        /// <summary>
+        /// The stopwatch which will be used to calculate the download speed
+        /// </summary>
+        Stopwatch m_StopWatch; 
+
+        public MainForm()
+        {
+            InitializeComponent();
+            // Initialize stopwatch
+            m_StopWatch = new Stopwatch();
+        }
+
+        // Handles click on 'Download' button
+        private void IDB_DOWNLOAD_Click(object sender, EventArgs e)
+        {
+            string filename = String.Empty;
+            string pathToSavedFile = String.Empty;
+
+            HideStatus();
+            try
+            {
+                Uri URL = new Uri(IDT_URL.Text);
+                // Get the name of file to be downloaded
+                filename = Path.GetFileName(URL.AbsolutePath);
+                // Verify that specified URI is a file
+                if (filename.Equals(String.Empty)) throw new Exception("Invalid URI: This is not a file");
+
+                pathToSavedFile = IDT_SAVETO.Text;
+                // Verify that pathToSavedFile to saved file is not empty
+                if (pathToSavedFile.Equals(String.Empty)) throw new Exception("Error: Path to saved file should not be empty");
+                // Verify that pathToSavedFile to saved file exists
+                if (!Directory.Exists(pathToSavedFile)) throw new Exception("Error: Path to saved file doesn't exist");
+
+                ShowStatus(string.Format("Filename: {0}\nSaved to: {1}", filename, pathToSavedFile), false);
+                EnableDownloadButton(false);
+
+                // Start downloading
+                DownloadFile(URL, pathToSavedFile + filename);
+            }
+            catch (Exception ex)
+            {
+                ShowStatus(ex.Message, false);
+            }
+        }
+
+        // Handles click on '...' button (choose path to saved file)
+        private void IDB_SAVETO_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                IDT_SAVETO.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        /// <summary>
+        /// Method starting async downloading of requested file
+        /// </summary>
+        /// <param name="url">URL of file to download</param>
+        /// <param name="location">Full path with name of the saved file</param>
+        public void DownloadFile(Uri url, string location)
+        {
+            using (m_WebClient = new WebClient())
+            {
+                m_WebClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                m_WebClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+
+                // Start the stopwatch which we will be using to calculate the download speed
+                m_StopWatch.Start();
+
+                try
+                {
+                    // Start downloading the file
+                    m_WebClient.DownloadFileAsync(url, location);
+                }
+                catch (Exception ex)
+                {
+                    // Show error message in status holder if any arised
+                    IDT_STATUS.Invoke((MethodInvoker)delegate { IDT_STATUS.Text = ex.Message; });
+                }
+            }
+        }
+
+        // The event that will fire whenever the progress of the WebClient is changed
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            // Download speed in bytes per second
+            double downloadSpeedBytes = e.BytesReceived / m_StopWatch.Elapsed.TotalSeconds;
+            // Calculate download speed in kb/s
+            IDT_TRANSFERRATE.Text = string.Format("{0} kb/s", (downloadSpeedBytes / 1024d).ToString("0.00"));
+
+            // Update the progressbar percentage only when the value is not the same.
+            IDPB_DOWNLOADPROGRESS.Value = e.ProgressPercentage;
+
+            // Calculate estimated time left to complete download.
+            IDT_TIMELEFT.Text = string.Format("{0} sec", 
+                ((e.TotalBytesToReceive - e.BytesReceived) / downloadSpeedBytes).ToString("0.00"));
+
+            // Show the amount of downloaded bytes
+            IDT_DOWNLOADEDBYTES.Text = string.Format("{0} MB's / {1} MB's",
+                (e.BytesReceived / 1024d / 1024d).ToString("0.00"),
+                (e.TotalBytesToReceive / 1024d / 1024d).ToString("0.00"));
+        }
+
+        // The event that will trigger when the WebClient is completed
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            // Reset the stopwatch.
+            m_StopWatch.Reset();
+            EnableDownloadButton(true);
+            if (e.Error != null)
+            {
+                string error = e.Error.ToString();
+                // Show error message in status holder if any arised
+                IDT_STATUS.Invoke((MethodInvoker)delegate { IDT_STATUS.Text = error; });
+                return;
+            }
+            
+            if (e.Cancelled == true)
+            {
+                IDT_STATUS.Invoke((MethodInvoker)delegate { IDT_STATUS.Text = "Download canceled"; });
+            }
+            else
+            {
+                IDT_STATUS.Invoke((MethodInvoker)delegate { IDT_STATUS.Text = "Download completed!"; });
+            }
+            
+        }
+
+        #region Tools
+        /// <summary>
+        /// Show message in status holder
+        /// </summary>
+        /// <param name="message">Message to show</param>
+        /// <param name="append">Flag showing whether message will be appended to any existing messages. If False will truncate all previous messages</param>
+        private void ShowStatus(string message, bool append)
+        {
+            if (!IDT_STATUS.Visible) IDT_STATUS.Visible = true;
+            if (append) IDT_STATUS.Text += message;
+            else IDT_STATUS.Text = message;
+        }
+
+        /// <summary>
+        /// Hide status holder
+        /// </summary>
+        private void HideStatus()
+        {
+            if (IDT_STATUS.Visible) IDT_STATUS.Visible = false;
+        }
+
+        /// <summary>
+        /// Enables/disables download button
+        /// </summary>
+        /// <param name="enabled">True to enable download button, false to disable</param>
+        private void EnableDownloadButton(bool enabled)
+        {
+            IDB_DOWNLOAD.Enabled = enabled;
+        }
+        #endregion
+
+        
+    }
+}
+
